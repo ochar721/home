0c7a600bc4bc5cec8b1c52c1c33bf68797b0bce0
Olivia Charles <ocharles92@gmail.com>
2021-02-17 11:12:52 -0500

    Initial Commit, Updated Jessie to read as renter1, to match the iteration pattern.

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -4,12 +4,12 @@ require './lib/renter'
 
 class RenterTest < Minitest::Test
   def test_it_exists
-    jessie = Renter.new("Jessie")
-    assert_instance_of Renter, jessie
+    renter1 = Renter.new("Jessie")
+    assert_instance_of Renter, renter1
   end
 
   def test_it_has_a_name
-    jessie = Renter.new("Jessie")
-    assert_equal "Jessie", jessie.name
+    renter1 = Renter.new("Jessie")
+    assert_equal "Jessie", renter1.name
   end
 end
