defecf3ff336103b94bca39ed45914f1e8892d44
Olivia Charles <ocharles92@gmail.com>
2021-02-17 11:36:21 -0500

    Created all tests for building.

--- test/building_test.rb
+++ test/building_test.rb
@@ -6,5 +6,48 @@ require './lib/renter'
 
 class BuildingTest < Minitest::Test
 
-  
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @renter1 = Renter.new("Aurora")
+    @renter2 = Renter.new("Tim")
+
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Building, @building
+    assert_equal [], @building.units
+  end
+
+  def test_it_can_add_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    assert_nil [@unit1, @unit2], @building.units
+  end
+
+  def test_it_starts_with_no_renters
+    assert_equal [], @building.renters
+  end
+
+  def test_it_can_add_renters
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    @unit1.add_renter(@renter1)
+    assert_equal ["Aurora"], @building.renters
+
+    @unit2.add_renter(@renter2)
+    assert_equal ["Aurora", "Tim"], @building.renters
+  end
+
+  def test_it_has_an_average_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    @unit1.add_renter(@renter1)
+    @unit2.add_renter(@renter2)
+    
+    assert_equal 1099.5, @building.average_rent
+  end
 end
